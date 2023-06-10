7009b199a1f79f3782dd0f647c545e30c8eedd45
Yaroslav Kashkarov <yaroslavkashkarov@gmail.com>
2023-06-10 18:01:56 +0200

    creating a file markup index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,15 @@
-(function ($) {  
+( function ( $ )
+{
 
-  $(document).ready(function() {  
-      $('body').niceScroll();
-  });
+  $( document ).ready( function ()
+  {
+    $( 'body' ).niceScroll();
+  } );
 
-  $('.header-menu__btn').click(function() {
-    $(this).toggleClass('active');
-    $('.header-menu__list').toggleClass('active');
-  });
+  $( '.header-menu__btn' ).click( function ()
+  {
+    $( this ).toggleClass( 'active' );
+    $( '.header-menu__list' ).toggleClass( 'active' );
+  } );
 
-})(jQuery);
+} )( jQuery );

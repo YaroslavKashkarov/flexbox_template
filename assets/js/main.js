ae56f19b5f357f9bee1fb587fd7e914c1877f028
Yaroslav Kashkarov <yaroslavkashkarov@gmail.com>
2023-06-13 12:01:41 +0200

    writing the style and visual design of the project

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,9 @@
-( function ( $ )
-{
-
-  $( document ).ready( function ()
-  {
-    $( 'body' ).niceScroll();
-  } );
-
-  $( '.header-menu__btn' ).click( function ()
-  {
-    $( this ).toggleClass( 'active' );
-    $( '.header-menu__list' ).toggleClass( 'active' );
-  } );
-
-} )( jQuery );
+(function ($) {
+  $(document).ready(function () {
+    $('body').niceScroll();
+  });
+  $('.header-menu__btn').click(function () {
+    $(this).toggleClass('active');
+    $('.header-menu__list').toggleClass('active');
+  });
+})(jQuery);

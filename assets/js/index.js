e10f6408d2b2ab95ee252a35bf042390a55825a1
Yaroslav Kashkarov <yaroslavkashkarov@gmail.com>
2023-06-14 11:07:28 +0200

    Updating the html, css, js project and connecting libraries

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+(function ($) {
+  $(document).ready(function () {
+    $('body').niceScroll();
+  });
+  $(document).ready(function () {
+    $('.header-menu__btn').on('click', function () {
+      $(this).toggleClass('-active');
+      $('.header-menu__list').toggleClass('-active');
+    });
+  });
+})(jQuery);
--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-(function ($) {
-  $(document).ready(function () {
-    $('body').niceScroll();
-  });
-  $('.header-menu__btn').click(function () {
-    $(this).toggleClass('active');
-    $('.header-menu__list').toggleClass('active');
-  });
-})(jQuery);
